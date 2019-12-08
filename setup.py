51b6b597fe76a7ce6962e6cc3d6b07f8c0093ab1
Justas Birgiolas <jbirgio@gmail.com>
2019-12-07 16:24:40 -0800

    Fixed pip missing .hoc file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LFPsimpy",
-    version="0.1",
+    version="0.1.1",
     author="Justas Birgiolas",
     author_email="justas@asu.edu",
     description="Zero-model-modification, MPI-compatible Python package for "
@@ -14,6 +14,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/justasb/LFPsimpy",
     packages=setuptools.find_packages(),
+    package_data = {
+        'LFPsimpy': ['LFPsimpy.hoc'],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
